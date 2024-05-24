6a5f56d019acf40802d1a6cc414168992a671998
Leo Malinen <107366630+leomalinen04@users.noreply.github.com>
2024-05-24 10:09:02 -0700

    Final change to the IP checker, change format, display on TKINTER, and minor output changes

--- IP+Proxy+Checker.py
+++ IP+Proxy+Checker.py
@@ -1,16 +1,17 @@
 import requests
 import ipaddress
+import tkinter as tkin
 
-# Function to get the geolocation of an IP address through IPAPI
-def geolocate_ip(ip_address):
+# Function to get the geolocation of an IP address through IPINFO
+def geolocateIp(ipAddress):
     try:
-        ip_obj = ipaddress.ip_address(ip_address)
+        ipObj = ipaddress.ip_address(ipAddress)
     except ValueError:
         print("Invalid IP. Please enter an address in IPV4 or IPV6 form.")
         return
 
-    api_url = f"https://ipinfo.io/{ip_address}/json"
-    response = requests.get(api_url)
+    apiUrl = f"https://ipinfo.io/{ipAddress}/json"
+    response = requests.get(apiUrl)
     data = response.json()
 
     # Get the data and if it does not work, output Unknown
@@ -20,25 +21,30 @@ def geolocate_ip(ip_address):
     latitude = data.get("loc", "Unknown").split(',')[0] if data.get("loc") else "Unknown"
     longitude = data.get("loc", "Unknown").split(',')[1] if data.get("loc") else "Unknown"
 
-    # Print statements to display the necessary information
-    print(f"IP Address: {ip_address}")
-    print(f"Country: {country}")
-    print(f"City: {city}")
-    print(f"Region: {region}")
-    print(f"Latitude: {latitude}")
-    print(f"Longitude: {longitude}")
+    return f"IP Address: {ipAddress}\n Country: {country}\n City: {city}\n Region: {region}\n Latitude: {latitude}\n Longitude: {longitude}"
 
 # Function to check if the IP address is a proxy
-def is_proxy(ip_address):
-    api_url = f"https://proxycheck.io/v2/{ip_address}"
-    response = requests.get(api_url)
+def isProxy(ipAddress):
+    apiUrl = f"https://proxycheck.io/v2/{ipAddress}"
+    response = requests.get(apiUrl)
     data = response.json()
-    proxy_status = data.get(ip_address, {}).get('proxy', "No")
+    proxyStatus = data.get(ipAddress, {}).get('proxy', "Not Detected")
+    ipType = data.get(ipAddress, {}).get('type', "Unknown")
 
-    print(f"Proxy: {proxy_status}")
+    return f"\nProxy: {proxyStatus} \nType: {ipType}"
 
 # Main function for the program
 if __name__ == "__main__":
-    user_ip = input("Enter an IP address: ")
-    geolocate_ip(user_ip)
-    is_proxy(user_ip)
+    userIp = input("Enter an IP address: ")
+    geolocationInfo = geolocateIp(userIp)
+    proxyInfo = isProxy(userIp)
+
+    # Create a simple Tkinter window
+    root = tkin.Tk()
+    root.title("IP, DNS, Geolocation and Proxy Checker")
+
+    # Create and display the TKinter text box with data
+    textBox = tkin.Text(root, bg="black", fg="white")
+    textBox.pack(fill="both", expand=True)
+    textBox.insert("end", geolocationInfo + "\n" + proxyInfo)
+    root.mainloop()
